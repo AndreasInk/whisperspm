2e7a08d6a2f439d4dcaea2c40e5eea06f40061fc
Andreas Ink <andreasink17@gmail.com>
2023-03-18 18:44:22 -0400

    lower package version

--- Package.swift
+++ Package.swift
@@ -1,17 +1,15 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
-    name: "whisperspm",
+    name: "whisper",
     products: [
         .library(
             name: "whisper",
             targets: ["whisper"])
     ],
     targets: [
-        .target(name: "whisper", dependencies:[], cSettings: [.unsafeFlags(["-O3", "-DGGML_USE_ACCELERATE=1"])]),
-        .target(name: "test-objc", dependencies:["whisper"]),
-        .target(name: "test-swift", dependencies:["whisper"])
+        .target(name: "whisper", dependencies:[], cSettings: [.unsafeFlags(["-O3", "-DGGML_USE_ACCELERATE=1"])])
     ],
     cxxLanguageStandard: CXXLanguageStandard.cxx11
 )
